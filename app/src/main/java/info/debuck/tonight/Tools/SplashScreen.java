88a8040950d796ba777d0eb4932f11518fc57e0d
steevex35 <Boulie15>
2016-05-17 23:13:51 +0200

    Splash Screen

---
+++ app/src/main/java/info/debuck/tonight/Tools/SplashScreen.java
@@ -0,0 +1,26 @@
+package info.debuck.tonight.Tools;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import info.debuck.tonight.MainActivity;
+
+
+/**
+ * Created by steeves on 14-05-16.
+ */
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+}
